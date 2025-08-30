711ea599d80895d2b7ff32401f5888788d7250fc
Ashir Mustafa <muhammadashirone@gmail.com>
2025-08-31 00:39:35 +0500

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Disable arrow keys in ALL modes like a true vim chad
+local modes = { "n", "i", "v", "x", "c" }
+
+for _, mode in pairs(modes) do
+  vim.keymap.set(mode, "<Up>", "<Nop>", { desc = "Disabled" })
+  vim.keymap.set(mode, "<Down>", "<Nop>", { desc = "Disabled" })
+  vim.keymap.set(mode, "<Left>", "<Nop>", { desc = "Disabled" })
+  vim.keymap.set(mode, "<Right>", "<Nop>", { desc = "Disabled" })
+end
+
+-- Optional: Show a message when someone tries to use them
+vim.keymap.set("n", "<Up>", function()
+  print("Use k, you coward!")
+end)
+vim.keymap.set("n", "<Down>", function()
+  print("Use j, you coward!")
+end)
+vim.keymap.set("n", "<Left>", function()
+  print("Use h, you coward!")
+end)
+vim.keymap.set("n", "<Right>", function()
+  print("Use l, you coward!")
+end)
